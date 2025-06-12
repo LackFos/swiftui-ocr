58631c3c87cb792fbe43dccf61fbe47d9e9c7083
LackFos <LackFosisOnline@gmail.com>
2025-06-12 12:09:32 +0700

    Initial Commit

---
+++ HaramApp/HaramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaramAppApp.swift
+//  HaramApp
+//
+//  Created by Elvis on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HaramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
